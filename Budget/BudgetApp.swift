623783242eb58dee2598b557ac3292a6262a752b
MB <mb6gm@virginia.edu>
2026-07-21 10:11:54 -0400

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Hannah Purvis on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTests/BudgetTests.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetTests.swift
+//  BudgetTests
+//
+//  Created by Hannah Purvis on 7/21/26.
+//
+
+import Testing
+
+struct BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
